44174075669dc4181ad109d3c480ca61bf97d333
dokinarieki <dokinarieki@disroot.org>
2026-05-06 19:46:43 +0300

    dasdasda

--- assets/js/main.js
+++ assets/js/main.js
@@ -119,9 +119,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
 	document.getElementById('mybtn').onclick = async () => {
 		await navigator.clipboard.writeText(`
-			<a href="https://rawr.rs/" target="_blank" style="image-rendering:pixelated">
-				<img src="https://rawr.rs/assets/88x31.png" alt="doeki's 88x31 button">
-			</a>	
+<a href="https://rawr.rs/" target="_blank" style="image-rendering:pixelated">
+	<img src="https://rawr.rs/assets/88x31.png" alt="doeki's 88x31 button">
+</a>	
 		`);
 	};
 });
